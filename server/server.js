307a17d88502db61dbbb4feee5da203219deaa69
James Meegan <58824024+jmeegan2@users.noreply.github.com>
2025-07-08 18:13:48 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { GoogleGenAI } = require("@google/genai");
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const apiKey = process.env.GEMINI_API_KEY;
+const ai = new GoogleGenAI({ apiKey });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+main();
